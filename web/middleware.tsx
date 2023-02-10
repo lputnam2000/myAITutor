b6d8635b0a82687c79d78a18525b2623cd63d188
Putnam, Lucien R <lucienp2@illinois.edu>
2023-02-10 01:42:00 -0600

    Middleware protect pages

---
+++ web/middleware.tsx
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "/pages/api/auth/[...nextauth]"
+import { getToken } from "next-auth/jwt";
+
+const requireAuth: string[] = ["/home"];
+const redirectAuth: string[] = [];
+
+export async function middleware(request: NextRequest) {
+  const res = NextResponse.next();
+  const pathname = request.nextUrl.pathname;
+  if (requireAuth.some((path) => pathname.startsWith(path))) {
+    const token = await getToken({
+      req: request,
+      secret: process.env.AUTH_SECRET,
+    });
+    //check not logged in
+    if (!token) {
+      const url = new URL(`/api/auth/signin`, request.url);
+      return NextResponse.redirect(url);
+    }
+    /*//check if not authorized
+    if (token.role !== "admin") {
+      const url = new URL(`/403`, request.url);
+      return NextResponse.rewrite(url);
+    }*/
+  }
+  if (redirectAuth.some((path) => pathname.startsWith(path))) {
+    const token = await getToken({
+      req: request,
+      secret: process.env.AUTH_SECRET,
+    });
+    //check if logged in
+    if (token) {
+      const url = new URL(`/home`, request.url);
+      return NextResponse.redirect(url);
+    }
+  }
+  return res;
+}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: 'tw-',
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
