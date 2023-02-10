82b38b3b4325a45f9cc0c7b808118d3c58f502ff
Putnam, Lucien R <lucienp2@illinois.edu>
2023-02-10 01:19:02 -0600

    Tailwind Install

---
+++ web/pages/home.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/UIComponents/Navbar";
+import React, { useRef, useState } from "react";
+
+export default function home() {
+    return(
+        <>
+            <Navbar/>
+            <div className="bg-green-500">hi</div>
+        </>
+    );
+}
