8819ed674ac2750f3b3d9dbd64474b175d230a71
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-02-13 20:56:56 -0600

    responsive home page

--- web/components/PDFViewerComponents/PDFViewer.jsx
+++ web/components/PDFViewerComponents/PDFViewer.jsx
@@ -7,7 +7,6 @@ import ViewerControls from "./ViewerControls";
 const StyledPage = styled(Page)`
   margin-bottom: 7px;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-
 `
 
 const Container = styled.div`
