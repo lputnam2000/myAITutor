60998a287561af78a1906aa87bfea490d767654e
Putnam, Lucien R <lucienp2@illinois.edu>
2023-03-02 16:24:41 -0600

    Update middleware.tsx

--- web/middleware.tsx
+++ web/middleware.tsx
@@ -16,7 +16,7 @@ export async function middleware(request: NextRequest) {
     //check not logged in
     
     if (!token) {
-      const redirectHref = request.nextUrl.host + `/signin`;
+      const redirectHref = request.nextUrl.basePath + `/signin`;
       console.log(redirectHref)
       return NextResponse.redirect(redirectHref);
     }
