dfc42cbb18246249e32f6e2674f31893d1f43a70
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-03-03 02:53:00 -0600

    CHI-30: basic home page with demo (#11)
    * change logo to h1
    
    * make basic homepage
    
    * basic home page ready

---
+++ web/pages/api/user/get_demo_pdf.js
@@ -0,0 +1,46 @@
+import clientPromise from "../../../lib/mongodb";
+import {getServerSession} from "next-auth/next"
+import {authOptions} from "pages/api/auth/[...nextauth]";
+import AWS from 'aws-sdk'
+
+const S3_BUCKET = process.env.CB_AWS_UPLOAD_BUCKET;
+const REGION = process.env.CB_AWS_REGION;
+const URL_EXPIRATION_TIME = 60 * 60 * 24; // in seconds
+AWS.config.update({signatureVersion: 'v4'})
+const myBucket = new AWS.S3({
+    accessKeyId: process.env.CB_AWS_ACCESS_ID,
+    secretAccessKey: process.env.CB_AWS_ACCESS_KEY,
+    params: {Bucket: S3_BUCKET},
+    region: REGION,
+})
+
+async function generatePreSignedGetUrl(key) {
+    return await myBucket.getSignedUrlPromise('getObject', {
+        Key: key,
+        Expires: URL_EXPIRATION_TIME,
+    })
+}
+
+const requestHandler = async (req, res) => {
+    if (req.method === "GET") {
+        const {key} = req.query
+        if (key !== 'f8e566b6-3590-4f3d-b48e-61c8ef7690ec') {
+            res.status(401).json({error: 'Unauthorised'})
+            return
+        }
+        if (key) {
+            let s3Url = await generatePreSignedGetUrl(key)
+            const client = await clientPromise;
+            const db = client.db("data");
+            const summaryDocuments = db.collection("SummaryDocuments");
+            const documentDetails = await summaryDocuments.findOne({_id: key});
+            return res.status(200).json({s3Url: s3Url, documentDetails: documentDetails})
+        } else {
+            res.status(400).json({'error': 'Invalid Key'})
+        }
+    } else {
+        return res.status(404).json({message: "URL Not Found"});
+    }
+};
+
+export default requestHandler;
