1e3e553d82c34da5c6afbd47f3caea389327cd73
Putnam, Lucien R <lucienp2@illinois.edu>
2023-03-06 19:21:16 -0600

    Persist api token

--- chrome/popup.js
+++ chrome/popup.js
@@ -33,12 +33,39 @@ document.addEventListener('DOMContentLoaded', function () {
                 files: ['content.js'],
             }, () => {
                 chrome.tabs.sendMessage(tab.id, { command: "getHTML" }, async function (response) {
-                    sendPage(token, await response)
+                    sendPage(token.value, await response)
                 });
             });
 
         });
 
     });
-    var apiKeyInput = document.getElementById('api-key');
+
+    token.value = getCookie("textValue");
+
+    // Add an event listener to save the input value to the cookie
+    token.addEventListener("input", function() {
+      setCookie("textValue", token.value, 30);
+    });
+
+    // Functions for setting and getting cookies
+    function setCookie(name, value, days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      const expires = "expires=" + date.toUTCString();
+      document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name) {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + "=")) {
+          return cookie.substring(name.length + 1);
+        }
+      }
+      return "";
+    }
+
+
 });
