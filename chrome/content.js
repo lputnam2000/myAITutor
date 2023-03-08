9fd1403281791617256f0109d908819babbfe31f
Putnam, Lucien R <lucienp2@illinois.edu>
2023-03-08 00:51:32 -0600

    Changed chrome extension payload schema, now includes website title
    The chrome extension will now send
    {
      title: 'http://... websitename',
      content: 'body html here'
    }
    
    to /api/chromeExtension/collectWeb

--- chrome/content.js
+++ chrome/content.js
@@ -1,6 +1,9 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.command === "getHTML")
-        sendResponse({content: document.body.innerHTML});
+        sendResponse({
+          title: document.URL,
+          content: document.body.innerHTML
+        });
     }
 );
