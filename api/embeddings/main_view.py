207de7af0b31b551f0a33e97a8a1d967807dbc5a
Aryaman Parekh <aparekh5@illinois.edu>
2023-03-07 23:36:33 -0600

    basic video embeddings

--- api/embeddings/main_view.py
+++ api/embeddings/main_view.py
@@ -3,6 +3,7 @@ from flask import Flask, Blueprint, jsonify, request
 from ..utils.utils import require_api_key, get_mongo_client, send_notification_to_client
 # from api.utils import require_api_key, get_mongo_client, send_notification_to_client
 from .websiteToEmbeddings import get_documents, get_client, create_class, upload_documents
+from .youtubeToEmbeddings import get_video_transcript, get_weaviate_docs
 
 embeddings_bp = Blueprint('embeddings', __name__, url_prefix='/embeddings')
 
@@ -30,6 +31,53 @@ def main_view():
         websitesCollection.update_one({"_id": key}, update_query)
         send_notification_to_client(user_id, key, f'Embeddings complete for:{key}')
         return jsonify({"message": "Embeddings Uploaded"}), HTTPStatus.OK
+    except Exception as e:
+        print(e)
+        raise e
+    
+@embeddings_bp.route('/youtube_video/', methods=['POST'])
+@require_api_key
+def main_view():
+    try:
+        data = request.json # .data is empty
+        url = data['url']
+        key = data['key']
+        formatted_subtitles = get_video_transcript(url)
+        db_client = get_mongo_client()
+        data_db = db_client["data"]
+        youtube_collection = data_db["SummaryYoutube"]
+        update_query = {"$set": {"status": "Ready", "transcript": formatted_subtitles}}
+        # Update the document matching the UUID with the new values
+        youtube_collection.update_one({"_id": key}, update_query)
+        documents = get_weaviate_docs(formatted_subtitles)
+        print('#PARSED DOCUMENTS')
+        client = get_client()
+        class_name = create_class(key, client)
+        print(f'#CREATED CLASS {class_name}')
+        upload_documents(documents, client, class_name)
+        print("#UPLOADED DOCUMENTS")
+        return jsonify({"message": "Embaddings Uploaded"}), HTTPStatus.OK
+        """
+        1. Download the Video, check availability
+        2. Get text from video using openai
+        3. Format the openai response, upload to mongodb
+        4. Format for Embeddings
+        5. Generate Embeddings
+        """
+        # documents = get_documents(url)
+        # print('#PARSED DOCUMENTS')
+        # client = get_client()
+        # class_name = create_class(key, client)
+        # print(f'#CREATED CLASS {class_name}')
+        # upload_documents(documents, client, class_name)
+        # print("#UPLOADED DOCUMENTS")
+        # db_client = get_mongo_client()
+        # data_db = db_client["data"]
+        # websitesCollection = data_db["SummaryWebsites"]
+        # update_query = {"$set": {"status": "Ready", "documents": documents}}
+        # # Update the document matching the UUID with the new values
+        # websitesCollection.update_one({"_id": key}, update_query)
+        # return jsonify({"message": "Embaddings Uploaded"}), HTTPStatus.OK
     except Exception as e:
         print(e)
         raise e
---
+++ api/embeddings/youtubeToEmbeddings.py
@@ -0,0 +1,126 @@
+from pytube import YouTube
+import os
+import uuid
+import openai
+import requests
+from datetime import datetime, timedelta
+import tiktoken
+from nltk import tokenize
+
+"""
+1. Download the Video, check availability
+2. Get text from video using openai
+3. Format the openai response, upload to mongodb
+4. Format for Embeddings
+5. Generate Embeddings
+"""
+
+ENCODER = tiktoken.get_encoding("gpt2")
+OPEN_AI_KEY = "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"
+openai.api_key = OPEN_AI_KEY
+WHISPER_MODEL_NAME = 'whisper-1'
+
+
+
+
+def get_weaviate_docs(transcripts):
+    tracripts_string = [t["text"] for t in transcripts]
+    encodings = ENCODER.encode_batch(tracripts_string)
+    to_return = []
+
+    for i in range(len(encodings)):
+        doc_length = len(encodings[i])
+        doc_text = ""
+        while doc_length < 300 and i+1 < len(encodings):
+            i += 1
+            doc_length += len(encodings[i]) 
+            doc_text += ' ' + tracripts_string[i]
+        to_return.append(doc_text)
+    return to_return
+
+def srt_to_array(srt_text):
+    # Split the SRT text into an array of subtitles
+    srt_array = srt_text.strip().split('\n\n')
+    subtitles = []
+
+    for s in srt_array:
+        # Split each subtitle into its timecodes and text
+        s_parts = s.split('\n')
+        # Extract start and end timecodes and convert to datetime objects
+        start_time = datetime.strptime(s_parts[1].split(' --> ')[0], '%H:%M:%S,%f')
+        end_time = datetime.strptime(s_parts[1].split(' --> ')[1], '%H:%M:%S,%f')
+        # Calculate start and end times in seconds
+        start_time_seconds = (start_time - datetime(1900, 1, 1)).total_seconds()
+        end_time_seconds = (end_time - datetime(1900, 1, 1)).total_seconds()
+        # Create a dictionary object with start and end times in seconds and text
+        subtitle = {'start': start_time_seconds, 'end': end_time_seconds, 'text': s_parts[2]}
+        subtitles.append(subtitle)
+    return subtitles
+
+def transcribe_file(model_id, path):
+    url = 'https://api.openai.com/v1/audio/transcriptions'
+    headers = {'Authorization': f'Bearer {OPEN_AI_KEY}'}
+    data = {'model': 'whisper-1',}
+    files = {
+        'file': open(path, 'rb'),
+        'model': (None, 'whisper-1'),
+        'response_format': (None, 'srt')
+    }
+    response = requests.post(url, headers=headers, files=files)
+    if response.ok:
+        return response.text
+    else:
+        # import pdb
+        # pdb.set_trace()
+        print(response.content.decode())
+        raise ValueError(f"Request failed with status code {response.status_code}.")
+
+
+
+def download_video(vidLink):
+    try:
+        vidObj = YouTube(vidLink)
+        vidObj.check_availability()
+    except Exception as e:
+        print(e)
+        return "vid not available"
+    vidStreams = vidObj.streams.filter(only_audio=True)[0]
+    if not vidStreams:
+        return "no streams availables"   
+    file_name = uuid.uuid4().__str__()
+    outFile = vidStreams.download(output_path='audio')
+    base, ext = os.path.splitext(outFile)
+    newFile = file_name + '.mp3'
+    os.rename(outFile, newFile)
+    return newFile
+
+def get_video_transcript(url):
+    print('#Downloading Video')
+    videoFile = download_video(url)
+    print('#Video Downloaded')
+    transcripts = transcribe_file(WHISPER_MODEL_NAME, videoFile)
+    formatted_subtitles = srt_to_array(transcripts)
+    print('#Transcripts Generated')
+    return formatted_subtitles
+
+def upload_to_weaviate(formatted_transcripts):
+    embedding_docs = get_weaviate_docs(formatted_transcripts)
+
+    print('#Downloading Video')
+    videoFile = download_video(url)
+    print('#Video Downloaded')
+    transcripts = transcribe_file(WHISPER_MODEL_NAME, videoFile)
+    formatted_subtitles = srt_to_array(transcripts)
+    print('#Transcripts Generated')
+    return formatted_subtitles
+
+
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=XALBGkjkUPQ"        
+    print('# Downloading Video')
+    videoFile = download_video(url)
+    print('#Video Downloaded')
+    transcripts = transcribe_file(WHISPER_MODEL_NAME, videoFile)
+    os.remove(videoFile)
+    formatted_subtitles = srt_to_array(transcripts)
+    print('#Transcripts Generated')
