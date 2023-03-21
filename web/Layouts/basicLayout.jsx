81c7ed4b4a3a9db387648d614cfd4bdb83fa69c0
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-03-20 20:16:42 -0500

    basic new navbar design (#31)

--- web/Layouts/basicLayout.jsx
+++ web/Layouts/basicLayout.jsx
@@ -1,21 +1,13 @@
 import Navbar from "../components/UIComponents/Navbar"
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 import styled from 'styled-components'
-import BackButton from '../components/UIComponents/BackButton'
 
-const StyledBack = styled.div`
-  margin: 4px 8px 4px 8px;
-`
-
-export default function BasicLayout({ children }) {
-  const router = useRouter();
-  return (
-    <>
-      <Navbar />
-      <StyledBack>
-        <BackButton/>
-      </StyledBack>
-      <main>{children}</main>
-    </>
-  )
+export default function BasicLayout({children}) {
+    const router = useRouter();
+    return (
+        <>
+            <Navbar/>
+            <main>{children}</main>
+        </>
+    )
 }
