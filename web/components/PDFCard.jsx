10c525b418c91463e041d3bbf167597c9956e4c7
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-03-21 00:32:04 -0500

    Aryamanparekh12/chi 72 pdf card (#32)
    * menu with remove working
    
    * rename works

--- web/components/PDFCard.jsx
+++ web/components/PDFCard.jsx
@@ -1,8 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components'
 import Link from 'next/link'
+import {
+    Button, IconButton, Menu, MenuButton, MenuItem, MenuList,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    Input,
+    FormControl,
+    FormLabel,
+} from "@chakra-ui/react";
+import {HiDotsVertical} from 'react-icons/hi'
+import {useRouter} from "next/router";
+import {DeleteIcon, EditIcon} from "@chakra-ui/icons";
+import axios from "axios";
+import {AnimatePresence, motion,} from 'framer-motion';
 
-const Container = styled(Link)`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 200px;
@@ -48,6 +66,7 @@ const CenteredText = styled.div`
 const TagList = styled.div`
   background-color: #fafdd4;
   display: flex;
+  justify-content: space-between;
 `
 const Tag = styled.div`
   margin: 10px;
@@ -66,8 +85,42 @@ const typeToLabel = {
     'youtube': 'YouTube'
 }
 
-function PdfCard({title, uploadId, thumbnail, type}) {
+function PdfCard({title, uploadId, thumbnail, type, onRemove, onRename}) {
+    // ...
+    const [isRenameModalOpen, setIsRenameModalOpen] = useState(false);
+    const [newTitle, setNewTitle] = useState(title);
+    const openRenameModal = (e) => {
+        e.stopPropagation();
+        setIsRenameModalOpen(true);
+    };
+
+    const closeRenameModal = () => {
+        setIsRenameModalOpen(false);
+    };
+    const renameUpload = (e) => {
+        e.stopPropagation();
+        console.log("New title:", newTitle);
+        // Perform your API call or other logic to update the title
+        onRename(uploadId, type, newTitle)
+        closeRenameModal();
+    };
+    const removeUpload = (e) => {
+        e.stopPropagation();
+
+        // Hide the card with the animation
+        setIsVisible(false);
+
+        // Remove the card after the animation duration
+        setTimeout(() => {
+            onRemove(uploadId, type);
+        }, exitAnimation.transition.duration * 1000);
+    };
+    // ...
+    const router = useRouter()
     const [thumbnailUrl, setThumbnailUrl] = useState('')
+    const [isVisible, setIsVisible] = useState(true);
+
+
     useEffect(() => {
         if (type === 'youtube') {
             const apiUrl = `https://noembed.com/embed?url=${encodeURIComponent(title)}`;
@@ -80,25 +133,90 @@ function PdfCard({title, uploadId, thumbnail, type}) {
         }
     }, [type])
 
-    return (
-        <Container href={`/summary?uploadId=${uploadId}&fileType=${type}`}>
-            {type === 'pdf' && <ImageContainer>
-                <img src={thumbnail} alt="" onerror={(e) => {
-                    e.target.style.display = "none"
-                }}/>
-            </ImageContainer>}
-            {type === 'youtube' && <ImageContainer>
-                <img src={thumbnailUrl} alt="" onerror={(e) => {
-                    e.target.style.display = "none"
-                }}/>
-            </ImageContainer>}
-            <CardInformation>
-                <CenteredText fileType={type}>{title}</CenteredText></CardInformation>
-            <TagList>
-                <Tag>{typeToLabel[type]}</Tag>
-            </TagList>
-        </Container>
-    );
+    const openSummary = () => {
+        router.push(`/summary?uploadId=${uploadId}&fileType=${type}`)
+        console.log('called')
+    }
+    const MotionContainer = motion(Container);
+
+    const exitAnimation = {
+        scale: 0,
+        opacity: 0,
+        transition: {
+            duration: 0.3,
+            ease: "easeInOut",
+        },
+    };
+
+
+    return (<>
+        {isVisible && (
+            <AnimatePresence>
+                <MotionContainer
+                    onClick={openSummary}
+                    exit={exitAnimation}
+                >
+                    {type === 'pdf' && <ImageContainer>
+                        <img src={thumbnail} alt="" onError={(e) => {
+                            e.target.style.display = "none"
+                        }}/>
+                    </ImageContainer>}
+                    {type === 'youtube' && <ImageContainer>
+                        <img src={thumbnailUrl} alt="" onError={(e) => {
+                            e.target.style.display = "none"
+                        }}/>
+                    </ImageContainer>}
+                    <CardInformation>
+                        <CenteredText fileType={type}>{title}</CenteredText></CardInformation>
+                    <TagList>
+                        <Tag>{typeToLabel[type]}</Tag>
+                        <Menu>
+                            <MenuButton
+                                as={IconButton}
+                                aria-label='Options'
+                                icon={<HiDotsVertical size={17}/>}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                }}
+                                variant='filled'
+                            />
+                            <MenuList>
+                                <MenuItem icon={<EditIcon/>} onClick={openRenameModal}>
+                                    Rename
+                                </MenuItem>
+                                <MenuItem onClick={removeUpload} icon={<DeleteIcon/>}>
+                                    Remove
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </TagList>
+                </MotionContainer>
+            </AnimatePresence>
+        )
+        }
+        <Modal isOpen={isRenameModalOpen} onClose={closeRenameModal}>
+            <ModalOverlay/>
+            <ModalContent>
+                <ModalHeader>Rename Title</ModalHeader>
+                <ModalCloseButton/>
+                <ModalBody>
+                    <FormControl>
+                        <FormLabel>New Title</FormLabel>
+                        <Input
+                            value={newTitle}
+                            onChange={(e) => setNewTitle(e.target.value)}
+                        />
+                    </FormControl>
+                </ModalBody>
+                <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={renameUpload}>
+                        Submit
+                    </Button>
+                    <Button onClick={closeRenameModal}>Cancel</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    </>);
 }
 
-export default PdfCard;
+export default React.memo(PdfCard);
