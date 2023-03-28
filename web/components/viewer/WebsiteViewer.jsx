63ec85a469ec44bdde4ee0f0d66fb8c87015acbe
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-03-27 20:54:18 -0500

    basic markdown viewer (#42)

--- web/components/viewer/WebsiteViewer.jsx
+++ web/components/viewer/WebsiteViewer.jsx
@@ -2,21 +2,65 @@ import React, {useContext, useEffect, useState} from 'react';
 import {ViewerContext} from "./context";
 import axios from "axios";
 import styled from 'styled-components'
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
-const StyledIframe = styled.iframe`
-  width: 90%;
-  margin: 5px 20px;
-  height: 750px;
-  border: 2px black solid;
 
-  &:hover {
-    box-shadow: 5px 5px 0px #000000;
-    transition: box-shadow 0.1s ease-in-out;
+const MarkdownWrapper = styled.div`
+  background-color: #282c34;
+  color: #ffffff;
+  padding: 20px;
+  max-width: 100%;
+  margin: 0 auto;
+  overflow: auto;
+  height: 80vh;
+  font-weight: 400;
+
+  h1, h2, h3 {
+    font-size: 24px;
   }
-`
+
+  h1, h2, h3, h4, h5, h6 {
+    color: #e692fd;
+  }
+
+  strong {
+    color: #A75EEFFF;
+    font-weight: 500;
+  }
+
+  code {
+    color: #60f616;
+    background-color: #111418;
+    padding: 0.1em 0.3em;
+    border-radius: 3px;
+  }
+
+  pre {
+    background-color: #111418;
+    padding: 1em;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 10px;
+    height: 300px;
+  }
+`;
 
 function WebsiteViewer() {
-    const [url, setUrl] = useState('');
+    const [markdown, setMarkdown] = useState(`
+# Hello World!
+
+Some text here with an [example link](https://example.com).
+
+![Placeholder image](images/placeholder-150.png)
+
+| Header 1    | Header 2    |
+| ----------- | ----------- |
+| Row 1, Cell 1 | Row 1, Cell 2 |
+| Row 2, Cell 1 | Row 2, Cell 2 |
+`);
     const {setSummary, setTitle, setFileType, pdfKey} = useContext(ViewerContext);
 
     const getDocumentDetails = (pdfKey) => {
@@ -31,10 +75,10 @@ function WebsiteViewer() {
         if (!pdfKey) return
         let params = {'key': pdfKey}
         axios.get('/api/user/get_url_document', {params: params}).then(res => {
-            setUrl(res.data.documentDetails.url)
             setSummary(res.data.documentDetails.summary)
             setTitle(res.data.documentDetails.title)
             setFileType(res.data.documentDetails.type)
+            setMarkdown(res.data.documentDetails.content)
         }).catch(err => {
             console.log(err)
         })
@@ -44,7 +88,9 @@ function WebsiteViewer() {
         }
     }, [pdfKey])
     return (
-        <StyledIframe src={url}></StyledIframe>
+        <MarkdownWrapper>
+            <ReactMarkdown remarkPlugins={[remarkGfm]} children={markdown}/>
+        </MarkdownWrapper>
     );
 }
 
