21d6be94393435d531adca7da9eac811b56e11f2
AshankKumar <ashankkumar01@gmail.com>
2023-03-28 20:17:08 -0500

    logging v1 done

--- api/embeddings/main_view.py
+++ api/embeddings/main_view.py
@@ -15,41 +15,53 @@ formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 @embeddings_bp.route('/websites/', methods=['POST'])
 @require_api_key
 def website_to_embedding():
+    stream_name = f'stream-name-website-embeddings-{str(uuid4())}'
+    new_handler = CloudWatchLogHandler(log_group_name='your-log-group-ashank', log_stream_name=stream_name)
+    new_handler.setFormatter(formatter)
+    current_app.logger.addHandler(new_handler)
     try:
         data = request.json
+        key = data['key']
         @copy_current_request_context
-        def run_in_context(data, function):
+        def run_in_context(data, function,stream_name):
             print("test")
-            function(data)
+            function(data,stream_name)
 
-        thread_embeddings = threading.Thread(target=run_in_context, args=(data,process_web_embeddings))
+        thread_embeddings = threading.Thread(target=run_in_context, args=(data,process_web_embeddings,stream_name))
         thread_embeddings.start()
+        current_app.logger.info(f'Processing website embeddings for {key}')
 
-        thread_text = threading.Thread(target=run_in_context, args=(data,process_web_text))
+
+        thread_text = threading.Thread(target=run_in_context, args=(data,process_web_text,stream_name))
         thread_text.start()
+        current_app.logger.info(f'Processing website text for {key}')
+
+        current_app.logger.removeHandler(new_handler)
         return jsonify({"message": "Request accepted, processing in background"}), HTTPStatus.ACCEPTED
     except Exception as e:
         print(e)
+        current_app.logger.info(f'Error:{e}')
+        current_app.logger.removeHandler(new_handler)
         raise e
 
 @embeddings_bp.route('/extension/', methods=['POST'])
 @require_api_key
 def extension_to_embedding():
+    stream_name = f'stream-name-extension-embeddings-{str(uuid4())}'
+    new_handler = CloudWatchLogHandler(log_group_name='your-log-group-ashank', log_stream_name=stream_name)
+    new_handler.setFormatter(formatter)
+    current_app.logger.addHandler(new_handler)
     try:
         data = request.json
         key = data['key']
         @copy_current_request_context
-        def run_in_context(data, function):
+        def run_in_context(data, function, stream_name):
             print("test")
-            function(data)
+            function(data,stream_name)
 
-        thread = threading.Thread(target=run_in_context, args=(data,process_chrome_extension_embeddings))
+        thread = threading.Thread(target=run_in_context, args=(data,process_chrome_extension_embeddings,stream_name))
         thread.start()
 
-        stream_name = f'stream-name-extension-embeddings-{str(uuid4())}'
-        new_handler = CloudWatchLogHandler(log_group_name='your-log-group-ashank', log_stream_name=stream_name)
-        new_handler.setFormatter(formatter)
-        current_app.logger.addHandler(new_handler)
         current_app.logger.info(f'Processing extension embeddings for {key}')
         current_app.logger.removeHandler(new_handler)
 
@@ -57,25 +69,27 @@ def extension_to_embedding():
 
     except Exception as e:
         print(e)
+        current_app.logger.info(f'Error:{e}')
+        current_app.logger.removeHandler(new_handler)
         raise e
 
 @embeddings_bp.route('/youtube_video/', methods=['POST'])
 @require_api_key
 def video_to_embedding():
+    stream_name = f'stream-name-video-embeddings-{str(uuid4())}'
+    new_handler = CloudWatchLogHandler(log_group_name='your-log-group-ashank', log_stream_name=stream_name)
+    new_handler.setFormatter(formatter)
+    current_app.logger.addHandler(new_handler)
     try:
         data = request.json
         key = data['key']
         @copy_current_request_context
-        def run_in_context(data, function):
+        def run_in_context(data, function,stream_name):
             print("test")
-            function(data)
-        thread = threading.Thread(target=run_in_context, args=(data,process_youtube_embeddings))
+            function(data,stream_name)
+        thread = threading.Thread(target=run_in_context, args=(data,process_youtube_embeddings,stream_name))
         thread.start()
 
-        stream_name = f'stream-name-video-embeddings-{str(uuid4())}'
-        new_handler = CloudWatchLogHandler(log_group_name='your-log-group-ashank', log_stream_name=stream_name)
-        new_handler.setFormatter(formatter)
-        current_app.logger.addHandler(new_handler)
         current_app.logger.info(f'Processing video embeddings for {key}')
         current_app.logger.removeHandler(new_handler)
 
@@ -83,4 +97,6 @@ def video_to_embedding():
 
     except Exception as e:
         print(e)
+        current_app.logger.info(f'Error:{e}')
+        current_app.logger.removeHandler(new_handler)
         raise e
