c7577d8f66e28e8a8577d9df9c2d3ebabd5d889e
Putnam, Lucien R <lucienp2@illinois.edu>
2023-03-30 02:36:37 -0500

    More changes to make the codebase more extensible

--- dataTether/run.py
+++ dataTether/run.py
@@ -1,5 +1,22 @@
-from app import server
+from flask import Flask
+from flask_socketio import SocketIO
+from config import Config
+from dataTether.app import events
+
+socketio = SocketIO()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    socketio.init_app(app)
+    register_blueprints(app)
+    return app
+
+def register_blueprints(app):
+    #app.register_blueprint(events.bp)
+    pass
+
+app = create_app()
 
 if __name__ == '__main__':
-    server.start_worker()
-    server.socketio.run(server.app, host='0.0.0.0', port=5000)
+    app.run()
