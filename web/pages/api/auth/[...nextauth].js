0c2adc19f1486109e0a76e1f262926957f124640
Lucien Putnam <lucienp2@illinois.edu>
2023-04-06 02:31:27 -0500

    Add Demo for new users (#71)

--- web/pages/api/auth/[...nextauth].js
+++ web/pages/api/auth/[...nextauth].js
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import clientPromise from "/lib/mongodb"
+import clientPromise from "../../../lib/mongodb";
+import {ObjectId} from 'mongodb';
 
 export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
@@ -24,6 +25,51 @@ export const authOptions = {
     }),
   ],
   callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        const client = await clientPromise;
+        let users = client.db("admin").collection("users");
+        let uid = user.user.id
+        let isRegistered = await users.findOne({
+          "_id": ObjectId(uid),
+          "Initialized": { $exists: true }
+        });
+        //check if the user is not registered
+        if (isRegistered == null) {
+          users.updateOne(
+            { "_id": ObjectId(uid) }, 
+            { $set: { "Initialized": true } }
+          )          
+          // do something if the user is new
+          console.log("NEW USER: ", uid)
+          let userUploads = client.db("data").collection("UserUploads");
+          const result = await userUploads.updateOne(
+            { userid: user.user.id },
+            {
+              $push: {
+                uploads:
+                {
+                  "uuid": "48a26389-ff79-4f66-a318-56d516544dcf",
+                  "title": "The Evolution of Apes :)",
+                  "status": "Not Ready",
+                  "type": "youtube",
+                  "url": "https://www.youtube.com/watch?v=5G13oKqCFs4"
+                }
+              },
+            },
+            { upsert: true }
+          );
+        } else {
+          console.log("OLD USER: ", user.user.id)
+        }
+      } catch (e) {
+        console.log("Error", e)
+      }
+      return true;
+
+
+
+    },
     session({ session, token }) {
       if (session.user) {
         session.user.id = token.sub;
