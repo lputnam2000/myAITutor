4c797d597d074ec8ba4377edaf78004b0aa98aef
Lucien Putnam <lucienp2@illinois.edu>
2023-04-05 20:08:11 -0500

    Chi 122 (#68)
    * Signin Fixes hopefully
    
    * Update signin.module.scss

--- web/pages/signin.js
+++ web/pages/signin.js
@@ -1,13 +1,14 @@
-import {getProviders, signIn, getCsrfToken} from "next-auth/react"
-import {getServerSession} from "next-auth/next"
-import {authOptions} from "./api/auth/[...nextauth]";
-import React, {useState} from "react"
-import {FcGoogle} from "react-icons/fc";
-import {AiOutlineMail} from "react-icons/ai"
+import { getProviders, signIn, getCsrfToken } from "next-auth/react"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./api/auth/[...nextauth]";
+import React, { useState } from "react"
+import { FcGoogle } from "react-icons/fc";
+import { AiOutlineMail } from "react-icons/ai"
 import styles from "/styles/signin.module.scss"
 import Link from "next/link";
-import styled from 'styled-components'
-import {FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, useToast} from "@chakra-ui/react";
+import styled, { keyframes } from "styled-components";
+import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, useToast } from "@chakra-ui/react";
+import { useRouter } from 'next/router';
 
 const NavbarContainer = styled.nav`
   height: 50px;
@@ -120,98 +121,173 @@ const TOSAgreement = styled.div`
 const TOSLink = styled(Link)`
   color: #52a1fc;
 `
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
-export default function SignIn({csrfToken, providers}) {
+const LargeSpinner = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 4rem;
+  height: 4rem;
+  border: 0.4rem solid rgba(0, 0, 0, 0.1);
+  border-left-color: #1a1a1a;
+  border-radius: 50%;
+  animation: ${rotate} 1.2s linear infinite;
+
+  &::before {
+    content: "⚡";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 1.5rem;
+  }
+`;
+
+export default function SignIn({ csrfToken, providers }) {
     const [isSignIn, setIsSignIn] = useState(true);
     const [email, setEmail] = useState('');
     const [isEmailInvalid, setIsEmailInvalid] = useState(false);
     const toast = useToast()
     const toastIdRef = React.useRef()
+    const router = useRouter();
+    const [formLoading, setFormLoading] = useState(false)
 
     const googleSignIn = async (e) => {
         e.preventDefault()
-        let googleProvider = Object.values(providers).filter(provider => provider.name == "Google")
-        signIn(googleProvider[0].id)
+        setFormLoading(true)
+        try {
+            let googleProvider = Object.values(providers).filter(provider => provider.name == "Google")
+            const response = await signIn(googleProvider[0].id)
+            if (response) {
+                const { error, status, ok, url} = response
+                if (ok) {
+                    console.log("signin success")
+                    router.push('/home')
+                } else {
+                    console.log(error)
+                    toastIdRef.current = toast({
+                        position: 'top',
+                        description: `Error with SignIn: ${error}. Sometimes Google OAuth errors are cause by already having made an account using the email box below.`
+                    })
+                }
+            }
+        } catch (e) {
+            console.log(e)
+            toastIdRef.current = toast({
+                position: 'top',
+                description: `Error with SignIn: ${e.message} \n`
+            })
+        }
+        setFormLoading(false)
     }
 
     const emailSignIn = async (e) => {
         e.preventDefault()
-        const emailRegex =
-            /^(?=[a-z0-9@._%+-]{6,254}$)[a-z0-9._%+-]{1,64}@(?:[a-z0-9-]{1,63}\.){1,8}[a-z]{2,63}$/;
-        if (!emailRegex.test(email.toLowerCase())) {
-            console.log('here')
-            setIsEmailInvalid(true)
-            return;
-        }
-
-        const response = await fetch(
-            `/api/auth/signin/email`,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                method: 'POST',
-                body: JSON.stringify({
-                    'email': email,
-                    'csrfToken': csrfToken,
-                }),
-            }).then((response) => {
-            if (response.ok) {
-                toastIdRef.current = toast({
-                    position: 'top',
-                    description: `Link Sent! Check Your Email for the Sign ${isSignIn ? 'In' : 'Up'} Link`
-                })
+        setFormLoading(true)
+        try {
+            const emailRegex =
+                /^(?=[a-z0-9@._%+-]{6,254}$)[a-z0-9._%+-]{1,64}@(?:[a-z0-9-]{1,63}\.){1,8}[a-z]{2,63}$/;
+            if (!emailRegex.test(email.toLowerCase())) {
+                console.log('here')
+                setIsEmailInvalid(true)
+                return;
             }
-        })
+            const response = await fetch(
+                `/api/auth/signin/email`,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'email': email,
+                        'csrfToken': csrfToken,
+                    }),
+                }).then((response) => {
+                    if (response.ok) {
+                        toastIdRef.current = toast({
+                            position: 'top',
+                            description: `Link Sent! Check Your Email for the Sign ${isSignIn ? 'In' : 'Up'} Link`
+                        })
+                    } else {
+                        toastIdRef.current = toast({
+                            position: 'top',
+                            description: `Sorry, there was an issue on our end. Refresh this page and try again.`
+                        })
+                    }
+                })
+        } catch (e) {
+            console.log(e)
+            toastIdRef.current = toast({
+                position: 'top',
+                description: `Error with SignIn: ${e.message} \n`
+            })
+        }
+        setFormLoading(false)
     };
 
-    const svg = <object className={styles.monkeyIcon} data={"/svg/monkey1.svg"}/>
     return (
         <Container>
-            <Navbar/>
+            <Navbar />
             <PageContainer>
-                {svg}
-                <SignInContainer>
-                    <TOSAgreement>
-                        By Signing {isSignIn ? 'In' : 'Up'}, you accept our &nbsp;
-                        <TOSLink href={'/tos'}>
-                            Terms of Service.
-                        </TOSLink>
-                    </TOSAgreement>
-                    <Button onClick={googleSignIn}>
-                        <FcGoogle size={22} style={{marginRight: '5px'}}/>
-                        Sign {isSignIn ? 'In' : 'Up'} with Google
-                    </Button>
-                    <Or>
-                        Or
-                    </Or>
-                    <EmailContainer>
-                        <FormControl isInvalid={isEmailInvalid} marginBottom={'10px'} id="email" isRequired>
-                            <FormLabel color={'#fff'}>Email address</FormLabel>
-                            <Input borderColor={'#57657e'}
+                {formLoading ?
+                    <>
+                        <LargeSpinner />
+                        <object className={styles.monkeyIcon} data={"/svg/monkey1.svg"} />
+                    </>
+                    :
+                    <>
+                        <object className={styles.monkeyIcon} data={"/svg/monkey1.svg"} />
+                        <SignInContainer>
+                            <TOSAgreement>
+                                By Signing {isSignIn ? 'In' : 'Up'}, you accept our &nbsp;
+                                <TOSLink href={'/tos'}>
+                                    Terms of Service.
+                                </TOSLink>
+                            </TOSAgreement>
+                            <Button onClick={googleSignIn}>
+                                <FcGoogle size={22} style={{ marginRight: '5px' }} />
+                                Sign {isSignIn ? 'In' : 'Up'} with Google
+                            </Button>
+                            <Or>
+                                Or
+                            </Or>
+                            <EmailContainer>
+                                <FormControl isInvalid={isEmailInvalid} marginBottom={'10px'} id="email" isRequired>
+                                    <FormLabel color={'#fff'}>Email address</FormLabel>
+                                    <Input borderColor={'#57657e'}
 
-                                   value={email}
-                                   onChange={e => setEmail(e.target.value)}
-                                   color={'#fff'} type="email" placeholder="Enter your email address"/>
-                            {!isEmailInvalid ? (
-                                <FormHelperText color={'#fff'}>
-                                    We&apos;ll never share your email.
-                                </FormHelperText>
-                            ) : (
-                                <FormErrorMessage>Invalid Email</FormErrorMessage>
-                            )}
-                        </FormControl>
+                                        value={email}
+                                        onChange={e => setEmail(e.target.value)}
+                                        color={'#fff'} type="email" placeholder="Enter your email address" />
+                                    {!isEmailInvalid ? (
+                                        <FormHelperText color={'#fff'}>
+                                            We&apos;ll never share your email.
+                                        </FormHelperText>
+                                    ) : (
+                                        <FormErrorMessage>Invalid Email</FormErrorMessage>
+                                    )}
+                                </FormControl>
 
-                        <Button onClick={emailSignIn}>
-                            <AiOutlineMail size={22} style={{marginRight: '5px'}}/>
-                            Sign {isSignIn ? 'In' : 'Up'} with Email
-                        </Button>
-                    </EmailContainer>
-                    <CreateAccount onClick={() => setIsSignIn(!isSignIn)}>
-                        {isSignIn ? 'Newbie? Let\'s Sign Up! 😎' :
-                            'Back to Sign In? 😁'}
-                    </CreateAccount>
-                </SignInContainer>
+                                <Button onClick={emailSignIn}>
+                                    <AiOutlineMail size={22} style={{ marginRight: '5px' }} />
+                                    Sign {isSignIn ? 'In' : 'Up'} with Email
+                                </Button>
+                            </EmailContainer>
+                            <CreateAccount onClick={() => setIsSignIn(!isSignIn)}>
+                                {isSignIn ? 'Newbie? Let\'s Sign Up! 😎' :
+                                    'Back to Sign In? 😁'}
+                            </CreateAccount>
+                        </SignInContainer>
+                    </>
+                }
             </PageContainer>
         </Container>
     )
@@ -226,12 +302,12 @@ export async function getServerSideProps(context) {
     // To avoid an infinite loop!
 
     if (session) {
-        return {redirect: {destination: "/home"}};
+        return { redirect: { destination: "/home" } };
     }
 
     const providers = await getProviders(context);
 
     return {
-        props: {providers: Object.values(providers) ?? [], csrfToken},
+        props: { providers: Object.values(providers) ?? [], csrfToken },
     }
 }
