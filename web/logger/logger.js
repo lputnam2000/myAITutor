b4baa56166d0f837cb211b15624752a1645a6ea5
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-04-08 21:43:02 -0500

    add logger now

---
+++ web/logger/logger.js
@@ -0,0 +1,28 @@
+import pino from 'pino'
+import {logflarePinoVercel} from 'pino-logflare'
+
+// create pino-logflare console stream for serverless functions and send function for browser logs
+// Browser logs are going to: https://logflare.app/sources/13989
+// Vercel log drain was setup to send logs here: https://logflare.app/sources/13830
+
+const {stream, send} = logflarePinoVercel({
+    apiKey: "FkAzU0rEGySb",
+    sourceToken: "9b4187a6-2b92-484e-8df6-72bd76f84443"
+});
+
+// create pino logger
+const logger = pino({
+    browser: {
+        transmit: {
+            level: "info",
+            send: send,
+        }
+    },
+    level: "debug",
+    base: {
+        env: process.env.NODE_ENV,
+        revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    },
+}, stream);
+
+export default logger
