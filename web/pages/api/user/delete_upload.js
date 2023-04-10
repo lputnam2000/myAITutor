bc58aadaa14d9a8fb73be28f69862f07523e2f57
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-04-10 14:02:02 -0500

    remove obj from s3, weaviate

--- web/pages/api/user/delete_upload.js
+++ web/pages/api/user/delete_upload.js
@@ -1,6 +1,23 @@
 import clientPromise from "../../../lib/mongodb";
 import {getServerSession} from "next-auth/next";
 import {authOptions} from "/pages/api/auth/[...nextauth]";
+import AWS from "aws-sdk";
+import weaviate from "weaviate-client";
+
+const weaviateClient = weaviate.client({
+    scheme: "https",
+    host: "chimpbase.weaviate.network",
+    authClientSecret: new weaviate.AuthUserPasswordCredentials({
+        username: "aryamanparekh12@gmail.com",
+        password: "pBCjEiL6GGN5fjQ",
+        scopes: ["offline_access"]  // optional, depends on the configuration of your identity provider (not required with WCS)
+    }),
+});
+
+const getWeaviateClassName = (key) => {
+    let key_fmtd = key.replaceAll('-', '_')
+    return `Document_${key_fmtd}`
+}
 
 const deleteUpload = async (req, res) => {
     const session = await getServerSession(req, res, authOptions)
@@ -18,9 +35,44 @@ const deleteUpload = async (req, res) => {
                         {'userid': session.user.id},
                         {$pull: {uploads: {'uuid': key}}}
                     );
+                    await weaviateClient.schema
+                        .classDeleter()
+                        .withClassName(getWeaviateClassName(key))
+                        .do()
+                        .then(res => {
+                            console.log(res);
+                        })
+                        .catch(err => {
+                            console.error(err)
+                        });
+
+                    AWS.config.update({signatureVersion: 'v4'})
+                    let S3_BUCKET = process.env.CB_AWS_UPLOAD_BUCKET;
+                    const REGION = process.env.CB_AWS_REGION;
+                    if (fileType === 'mp4') {
+                        S3_BUCKET = process.env.CB_AWS_VIDEO_UPLOAD_BUCKET
+                    }
+                    const awsParams = {
+                        Bucket: S3_BUCKET,
+                        Key: key
+                    };
+                    const myBucket = new AWS.S3({
+                        accessKeyId: process.env.CB_AWS_ACCESS_ID,
+                        secretAccessKey: process.env.CB_AWS_ACCESS_KEY,
+                        params: {Bucket: S3_BUCKET},
+                        region: REGION,
+                    })
+
                     if (fileType === 'pdf') {
                         const collection = db.collection('SummaryDocuments');
                         const r = await collection.deleteOne({_id: key});
+                        myBucket.deleteObject(awsParams, function (err, data) {
+                            if (err) {
+                                console.log(err, err.stack);
+                            } else {
+                                console.log('Object deleted successfully');
+                            }
+                        });
                     } else if (fileType === 'url') {
                         const collection = db.collection('SummaryWebsites');
                         const r = await collection.deleteOne({_id: key});
@@ -30,6 +82,13 @@ const deleteUpload = async (req, res) => {
                     } else if (fileType === 'mp4') {
                         const collection = db.collection('SummaryVideos');
                         const r = await collection.deleteOne({_id: key});
+                        myBucket.deleteObject(awsParams, function (err, data) {
+                            if (err) {
+                                console.log(err, err.stack);
+                            } else {
+                                console.log('Object deleted successfully');
+                            }
+                        });
                     }
                 } catch (e) {
                     res.status(500).json({error: e})
