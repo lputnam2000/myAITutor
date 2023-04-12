0d86b5879e19ee0b7e8bdaae2bfa74531e9bde87
Lucien Putnam <lucienp2@illinois.edu>
2023-04-12 02:00:15 -0500

    Update delete_upload.js (#79)

--- web/pages/api/user/delete_upload.js
+++ web/pages/api/user/delete_upload.js
@@ -1,6 +1,6 @@
 import clientPromise from "../../../lib/mongodb";
-import {getServerSession} from "next-auth/next";
-import {authOptions} from "/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "/pages/api/auth/[...nextauth]";
 import AWS from "aws-sdk";
 import weaviate from "weaviate-client";
 
@@ -24,7 +24,7 @@ const deleteUpload = async (req, res) => {
     const body = req.body;
     if (req.method === "DELETE") {
         if (session) {
-            const {key, fileType} = req.query
+            const { key, fileType } = req.query
             if (key) {
                 console.log(`Deleting Upload: ${key}`)
                 try {
@@ -32,79 +32,84 @@ const deleteUpload = async (req, res) => {
                     const db = client.db("data");
                     const uploadsCollection = db.collection("UserUploads");
                     const result = await uploadsCollection.updateOne(
-                        {'userid': session.user.id},
-                        {$pull: {uploads: {'uuid': key}}}
+                        { 'userid': session.user.id },
+                        { $pull: { uploads: { 'uuid': key } } }
                     );
-                    await weaviateClient.schema
-                        .classDeleter()
-                        .withClassName(getWeaviateClassName(key))
-                        .do()
-                        .then(res => {
-                            console.log(res);
+                    if (key == "103db47b-e6a7-4c5a-a543-e9f40062b0dc" || key == "d6190bcb-0bc6-4464-89c8-a1431db4546a") {
+                        console.log("did not actually delete")
+                        res.status(400).json({ 'Exception': 'Could not delete; this is a demo document' })
+                    } else {
+                        await weaviateClient.schema
+                            .classDeleter()
+                            .withClassName(getWeaviateClassName(key))
+                            .do()
+                            .then(res => {
+                                console.log(res);
+                            })
+                            .catch(err => {
+                                console.error(err)
+                            });
+
+                        AWS.config.update({ signatureVersion: 'v4' })
+                        let S3_BUCKET = process.env.CB_AWS_UPLOAD_BUCKET;
+                        const REGION = process.env.CB_AWS_REGION;
+                        if (fileType === 'mp4') {
+                            S3_BUCKET = process.env.CB_AWS_VIDEO_UPLOAD_BUCKET
+                        }
+                        const awsParams = {
+                            Bucket: S3_BUCKET,
+                            Key: key
+                        };
+                        const myBucket = new AWS.S3({
+                            accessKeyId: process.env.CB_AWS_ACCESS_ID,
+                            secretAccessKey: process.env.CB_AWS_ACCESS_KEY,
+                            params: { Bucket: S3_BUCKET },
+                            region: REGION,
                         })
-                        .catch(err => {
-                            console.error(err)
-                        });
 
-                    AWS.config.update({signatureVersion: 'v4'})
-                    let S3_BUCKET = process.env.CB_AWS_UPLOAD_BUCKET;
-                    const REGION = process.env.CB_AWS_REGION;
-                    if (fileType === 'mp4') {
-                        S3_BUCKET = process.env.CB_AWS_VIDEO_UPLOAD_BUCKET
-                    }
-                    const awsParams = {
-                        Bucket: S3_BUCKET,
-                        Key: key
-                    };
-                    const myBucket = new AWS.S3({
-                        accessKeyId: process.env.CB_AWS_ACCESS_ID,
-                        secretAccessKey: process.env.CB_AWS_ACCESS_KEY,
-                        params: {Bucket: S3_BUCKET},
-                        region: REGION,
-                    })
-
-                    if (fileType === 'pdf') {
-                        const collection = db.collection('SummaryDocuments');
-                        const r = await collection.deleteOne({_id: key});
-                        myBucket.deleteObject(awsParams, function (err, data) {
-                            if (err) {
-                                console.log(err, err.stack);
-                            } else {
-                                console.log('Object deleted successfully');
-                            }
-                        });
-                    } else if (fileType === 'url') {
-                        const collection = db.collection('SummaryWebsites');
-                        const r = await collection.deleteOne({_id: key});
-                    } else if (fileType === 'youtube') {
-                        const collection = db.collection('SummaryYoutube');
-                        const r = await collection.deleteOne({_id: key});
-                    } else if (fileType === 'mp4') {
-                        const collection = db.collection('SummaryVideos');
-                        const r = await collection.deleteOne({_id: key});
-                        myBucket.deleteObject(awsParams, function (err, data) {
-                            if (err) {
-                                console.log(err, err.stack);
-                            } else {
-                                console.log('Object deleted successfully');
-                            }
-                        });
+                        if (fileType === 'pdf') {
+                            const collection = db.collection('SummaryDocuments');
+                            const r = await collection.deleteOne({ _id: key });
+                            myBucket.deleteObject(awsParams, function (err, data) {
+                                if (err) {
+                                    console.log(err, err.stack);
+                                } else {
+                                    console.log('Object deleted successfully');
+                                }
+                            });
+                        } else if (fileType === 'url') {
+                            const collection = db.collection('SummaryWebsites');
+                            const r = await collection.deleteOne({ _id: key });
+                        } else if (fileType === 'youtube') {
+                            const collection = db.collection('SummaryYoutube');
+                            const r = await collection.deleteOne({ _id: key });
+                        } else if (fileType === 'mp4') {
+                            const collection = db.collection('SummaryVideos');
+                            const r = await collection.deleteOne({ _id: key });
+                            myBucket.deleteObject(awsParams, function (err, data) {
+                                if (err) {
+                                    console.log(err, err.stack);
+                                } else {
+                                    console.log('Object deleted successfully');
+                                }
+                            });
+                        }
                     }
                 } catch (e) {
-                    res.status(500).json({error: e})
+                    res.status(500).json({ error: e })
                     console.error(e);
                 }
 
             } else {
-                res.status(400).json({'error': 'Invalid Key'})
+                res.status(400).json({ 'error': 'Invalid Key' })
             }
         } else {
             // Not Signed in
             res.status(401)
-            res.json({"Error": "No authentication"})
+            res.json({ "Error": "No authentication" })
         }
     } else {
-        return res.status(404).json({message: "URL Not Found"});
+        return res.status(404).json({ message: "URL Not Found" });
     }
     res.end();
 };
