29c29b9ba4e2ccb1e749805e94d58d1bc8132278
Aryaman Parekh <aryamanparekh12@gmail.com>
2023-04-19 17:35:17 -0500

    fix settings

--- web/pages/settings.js
+++ web/pages/settings.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 import Layout from "../Layouts/basicLayout"
 import styled from 'styled-components'
 import Sidebar from "../components/settings/Sidebar";
@@ -6,7 +6,7 @@ import Section from "../components/settings/Section";
 import UserInformation from "../components/settings/sections/UserInformation";
 import ExtensionInformation from "../components/settings/sections/ExtensionInformation";
 import SettingsContextProvider from "../components/settings/context";
-import { NextSeo } from "next-seo";
+import {NextSeo} from "next-seo";
 
 const Container = styled.div`
   display: flex;
@@ -32,66 +32,59 @@ const SidebarWrapper = styled.div`
 `
 
 const InstallExtensionLink = styled.a`
-  margin: 0 1rem 0 0;
-  color: #bb6bd9;
+  margin: 10px 0px;
+  color: #80fadd;
+  font-weight: 300;
   text-decoration: underline;
-  font-weight: bold;
   transition: color 0.3s ease-in-out;
 
   &:hover {
-    color: #e068e1;
+    color: #48f8ce;
   }
 `;
 
 const Settings = () => {
-  const userInfoRef = useRef(null);
-  const extensionKeyRef = useRef(null);
+    const userInfoRef = useRef(null);
+    const extensionKeyRef = useRef(null);
 
-  const scrollToSection = (ref) => {
-    ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
+    const scrollToSection = (ref) => {
+        ref.current.scrollIntoView({behavior: "smooth", block: "start"});
+    };
 
-  return (
-    <Container>
-      <NextSeo
-        title="Settings"
-        description="Chimpbase Settings Page - Chimpbase is a game-changing information discovery platform, leveraging advanced AI technologies such as semantic search and chatGPT to deliver accurate, efficient and comprehensive insights from various sources including videos, documents, and websites. With AI-powered summarization and intuitive interfaces, Chimpbase is the ultimate solution for users seeking to deepen their understanding and knowledge."
-      />
-      <SettingsContextProvider>
-        <SidebarWrapper>
-          <Sidebar
-            onUserInfoClick={() => scrollToSection(userInfoRef)}
-            onExtensionKeyClick={() => scrollToSection(extensionKeyRef)} />
-        </SidebarWrapper>
-        <Content>
-          <Section
-            ref={userInfoRef}
-            title="User Information"
-          ><UserInformation /></Section>
-          <Section
-            ref={extensionKeyRef}
-            title="Extension Key"
-          >
-            <ExtensionInformation />
-            <InstallExtensionLink
-              href="https://chrome.google.com/webstore/detail/chimpbase-page-collector/igcljiaagpkbcamaceaekidclckkmnkp"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Download Extension Here
-            </InstallExtensionLink>
-            <InstallExtensionLink
-              href="https://blog.chimpbase.com/blog/how-to-use-chimpbase-browser-extension-for-website-summarization"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Tutorial
-            </InstallExtensionLink>
-          </Section>
-        </Content>
-      </SettingsContextProvider>
-    </Container>
-  );
+    return (
+        <Container>
+            <NextSeo
+                title="Settings"
+                description="Chimpbase Settings Page - Chimpbase is a game-changing information discovery platform, leveraging advanced AI technologies such as semantic search and chatGPT to deliver accurate, efficient and comprehensive insights from various sources including videos, documents, and websites. With AI-powered summarization and intuitive interfaces, Chimpbase is the ultimate solution for users seeking to deepen their understanding and knowledge."
+            />
+            <SettingsContextProvider>
+                <SidebarWrapper>
+                    <Sidebar
+                        onUserInfoClick={() => scrollToSection(userInfoRef)}
+                        onExtensionKeyClick={() => scrollToSection(extensionKeyRef)}/>
+                </SidebarWrapper>
+                <Content>
+                    <Section
+                        ref={userInfoRef}
+                        title="User Information"
+                    ><UserInformation/></Section>
+                    <Section
+                        ref={extensionKeyRef}
+                        title="Extension Key"
+                    >
+                        <ExtensionInformation/>
+                        <InstallExtensionLink
+                            href="https://chrome.google.com/webstore/detail/chimpbase-page-collector/igcljiaagpkbcamaceaekidclckkmnkp"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Our Chrome Extension lets you grab info from any website you visit with just one click!
+                        </InstallExtensionLink>
+                    </Section>
+                </Content>
+            </SettingsContextProvider>
+        </Container>
+    );
 };
 
 Settings.PageLayout = Layout;
